628d9560e578825a503e2b77921bc95da22d1acc
Edgar <edgargaticacu@gmail.com>
2023-04-26 09:38:18 -0500

    Adding static evidence and feedback components

---
+++ src/app/evidence/evidence.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evidence',
+  templateUrl: './evidence.component.html',
+  styleUrls: ['./evidence.component.css']
+})
+export class EvidenceComponent {
+
+}
